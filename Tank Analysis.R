6dc1755dac97a69b5d508e0ec0c0bfbc186cc5db
Atan1988 <tzw0423@hotmail.com>
2017-11-27 08:11:13 -0500

    model update

--- Tank Analysis.R
+++ Tank Analysis.R
@@ -155,6 +155,37 @@ system.time(fit6 <- gam(cbind(Relct, NRelct) ~ s(AvgAge) + FY + AvgCapacity + Is
                     data = facility_all, family = binomial(link = "logit")))
 
 
+system.time(fit7 <- gam(cbind(Relct, NRelct) ~ s(AvgAge) + FY + AvgCapacity + IsSensitiveArea_Yes +
+                          Contents_Gasoline + Contents_Diesel +
+                          PipingStyle_PPressure + PipingStyle_SSuction +
+                          PipingConstruction_BMBareMetal +
+                          PipingConstruction_FRPFiberglassReinforcedPlastic +
+                          PipingConstruction_FPTPFlexiblePlasticTechnologyPiping +  
+                          USTPrimaryReleaseDetection_ATGAutomaticTankGauging + 
+                          USTPrimaryReleaseDetection_AMOAlternativeMethodOtherexplain +
+                          USTPrimaryReleaseDetection_AMSIRAlternativeMethodSIR +
+                          # USTPrimaryReleaseDetection_IMOIntMonitoringOther + 
+                          # USTPrimaryReleaseDetection_IMTIntMonitoringDWTank +
+                          # USTPrimaryReleaseDetection_MTGHManualTankGauging5512000 + 
+                          # USTPrimaryReleaseDetection_MTGLManualTankGauging550 +
+                          USTPrimaryReleaseDetection_NPNonePresent +
+                          USTPrimaryReleaseDetection_NRNoneRequiredbyRule +
+                          USTConstruction_BMBareMetal + 
+                          USTConstruction_CCPSCoatedCathodicallyProtectedSteel +
+                          USTConstruction_FRPFiberglassReinforcedPlastic +
+                          # USTConstruction_DWFRPFiberglass + 
+                          # USTConstruction_CSCladSteel + 
+                          # USTConstruction_DWCladSteel +
+                          # USTConstruction_JSJacketedSteel + 
+                          # USTConstruction_SWCladSteel + 
+                          # USTConstruction_SWFRPFiberglass +
+                          PipingReleaseDetection_ELLDElectronicLineLeakDetector + 
+                          PipingReleaseDetection_MLLDMechanicalLineLeakDetector + 
+                          PipingReleaseDetection_SSSafeSuction +
+                          PipingReleaseDetection_OTHOtherexplain 
+                          , data = facility_all, family = binomial(link = "logit")))
+
+
 
 
 table(Tanks_df$IsSensitiveArea)
